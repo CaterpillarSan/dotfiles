2809fa9c210a22d0b23bfd8d34925fb9e57574d7
h K <caterpillar@macbookpro.local>
2023-06-13 04:10:32 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -x
 
-if [ $1 = 'macos' ]; then
+OS=undefined
+if [ `uname` == 'Darwin' ]; then
+  OS=Mac
+elif [ `expr substr $(uname -s) 1 5` eq 'Linux']; then
+  OS=Linux # TODO Ubuntu, CentOS
+fi
+
+
+if [ $OS == 'Mac' ]; then
 	# Mac OS
+
+	# Finder
+        defaults write com.apple.finder AppleShowAllFiles TRUE
+	# killall Finder
 
 	# Check Homebrew
 	if [ ! `which brew`]; then
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		eval "$(/opt/homebrew/bin/brew shellenv)"
 		brew -v || (echo "Failed to install brew." && exit 1)
 	fi
 
@@ -17,24 +30,22 @@ if [ $1 = 'macos' ]; then
 	fi
 
 
-	# Bash completion
 	brew install bash-completion
-	brew install git
-	brew install reattach-to-user-namespace
-	brew install fzf
-	brew install gnu-sed
-	brew install tmuxinator
+
+	git reattach-to-user-namespace fzf gnu-sed
+
 	# font
 	brew tap sanemat/font
 	brew install ricty
 	cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts
 	fc-cache -vf
 
-	brew install bat tree cmake gnu-sed ctags
+	brew install bat tree cmake gnu-sed ctags vim asdf
 
-	# Vim with lua
-	brew install vim --with-lua --with-override-system-vi
-elif [ $1 = "ubuntu" ]; then
+	# GUI
+  brew install --cask karabiner-elements google-chrome
+
+elif [ OS eq "Linux" ]; then
 	apt update
 	apt upgrade
 	apt install bash-completion
@@ -44,26 +55,34 @@ elif [ $1 = "ubuntu" ]; then
 	apt install bat tree cmake
 	apt install vim-gtk3
 else
-	echo "please input your distributution. macos/ubuntu"
+	echo "unknown OS"
 	exit 2
 fi
-## Neobundle
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+
+## asdf
+asdf plugin add nodejs
+asdf plugin add ruby
+asdf plugin add golang
+
+## Vim plugin
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # alias
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/bash_alias ~/.bash_alias
-ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/ideavimrc ~/.ideavimrc
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/tmux.conf.macos ~/.tmux.conf.macos
-ln -s ~/dotfiles/tmux.conf.ubuntu ~/.tmux.conf.ubuntu
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/eslintrc.json ~/.eslintrc.json
-ln -s ~/dotfiles/ocamlinit ~/.ocamlinit
-cp -r ~/dotfiles/vim ~/.vim
-cp -r ~/dotfiles/tmux ~/.tmux
-touch ~/.bash_env
+# ln -s ~/dotfiles/bash_profile ~/.bash_profile
+# ln -s ~/dotfiles/bash_alias ~/.bash_alias
+# ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
+# ln -s ~/dotfiles/ideavimrc ~/.ideavimrc
+# ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+# ln -s ~/dotfiles/tmux.conf.macos ~/.tmux.conf.macos
+# ln -s ~/dotfiles/tmux.conf.ubuntu ~/.tmux.conf.ubuntu
+# ln -s ~/dotfiles/vimrc ~/.vimrc
+# ln -s ~/dotfiles/eslintrc.json ~/.eslintrc.json
+# ln -s ~/dotfiles/ocamlinit ~/.ocamlinit
+# cp -r ~/dotfiles/vim ~/.vim
+# cp -r ~/dotfiles/tmux ~/.tmux
+# touch ~/.bash_env
 
 # tmux-plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
